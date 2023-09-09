f315990a39c5d1bc18b0a05d0baf1af99e088e58
Anil Devidas Janjal <132841194+AnilJanjal@users.noreply.github.com>
2023-09-09 18:14:08 +0530

    Update App.js
    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,32 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+export default function App() {
+  const [theme, setTheme] = useState("light"); 
 
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [text, setText] = useState({
-    buttonText: "Switch to Dark Mode",
-    h1text: "Light Mode Activated"
-  })
-
-  const light = { backgroundColor: "rgb(255, 255, 255)", color: "rgb(0, 0, 0)" };
-  const dark = { backgroundColor: "rgb(0, 0, 0)", color: "rgb(255, 255, 255)" };
-
-  const handelClick = () => {
-    if (!isDarkMode) {
-      setIsDarkMode(true);
-      const nObj = {
-        ...text,
-        buttonText: "Switch to Light Mode",
-        h1text: "Dark Mode Activated"
-      }
-      setText(nObj);
-
-    } else {
-      setIsDarkMode(false);
-      const nObj = {
-        ...text,
-        buttonText: "Switch to Dark Mode",
-        h1text: "Light Mode Activated"
-      }
-      setText(nObj);
-
-    }
-  }
+  const handleThemeToggle = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+  
 
   return (
-    <div id="main" style={isDarkMode ? dark : light}>
-      <h1>{text.h1text}</h1>
-      <button onClick={handelClick}>{text.buttonText}</button>
+    <div className="App" id={theme === "light" ? "light" : "dark"}>
+
+      <h1>Newton School</h1>
+      <div>
+        <form>
+          <label>UserName</label>
+          <input></input>
+          <label>Password</label>
+          <input></input>
+          <button>Login</button>
+        </form>
+      </div>
+      <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+      <input type="checkbox" id="switch" onChange={handleThemeToggle} />
+      <label htmlFor="switch" className="label">
+        Toggle
+      </label>
     </div>
-  )
+  );
 }
-
-
-export default App;
